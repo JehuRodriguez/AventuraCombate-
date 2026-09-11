2eb732d902c52677991f61711fa15e22bcef3341
Engel Indo <engelnickolas@gmail.com>
2026-09-10 22:20:06 -0500

    Created Melee Enemy

---
+++ AventuraCombate/MeleeEnemy.cs
@@ -0,0 +1,30 @@
+﻿public class MeleeEnemy
+{
+    private int vida;
+    private int dano;
+
+    public MeleeEnemy(int vidaInicial, int danoInicial)
+    {
+        this.vida = vidaInicial;
+        this.dano = danoInicial;
+    }
+
+    public void RecibirDano(int cantidad)
+    {
+        vida -= cantidad;
+        if (vida < 0)
+        {
+            vida = 0;
+        }
+    }
+
+    public int ObtenerDano()
+    {
+        return dano;
+    }
+
+    public bool EstaVivo()
+    {
+        return vida > 0;
+    }
+}
